e39ad29438657217dcee763cb90d7cbb5ebd00f4
加藤 史也 <FumKATO@katou-no-MacBook-Air.local>
2014-05-23 18:51:17 +0900

    Move cop.js

--- cop.js
+++ cop.js
@@ -6,10 +6,6 @@ Context = function(name, operations){
 		return {
 			adapt: function(object, functionName, operationName) {
 				//TODO: enable check context's validation
-				/*var command = object + '.prototype.proceed = ' + object + '.prototype.' + functionName + ';' +
-				  object + '.prototype.' + functionName + ' = operations.' + operationName + ';console.log(' +
-				  object + '.prototype.proceed); console.log(' + object + '.prototype.' + functionName + ');' + 
-				  'console.log(' + object + '.prototype.' + functionName + ');';*/
 				var command = 'var tmp = {' +
 					'proceed: ' + object + '.prototype.' + functionName + ',' +
 					functionName + ': operations.' + operationName +
